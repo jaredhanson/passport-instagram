286780199fb76fd12b323e37d64afff1894b1adc
Jared Hanson <jaredhanson@gmail.com>
2013-01-24 20:30:12 -0800

    Wrap error for informative message.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -101,6 +101,9 @@ vows.describe('InstagramStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
