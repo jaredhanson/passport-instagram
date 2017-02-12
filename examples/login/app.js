fc883d031aaa0630d7b19a2db681ef6de12b5140
Proustibat <jennifer.proust@gmail.com>
2017-02-12 20:46:54 +0100

    use express 4 instead of express 3, add partials module

--- examples/login/app.js
+++ examples/login/app.js
@@ -1,10 +1,22 @@
-var express = require('express')
-  , passport = require('passport')
-  , util = require('util')
-  , InstagramStrategy = require('passport-instagram').Strategy;
 
-var INSTAGRAM_CLIENT_ID = "--insert-instagram-client-id-here--"
-var INSTAGRAM_CLIENT_SECRET = "--insert-instagram-client-secret-here--";
+var express = require('express')
+    , passport = require('passport')
+    , morgan = require('morgan')
+    , cookieParser = require('cookie-parser')
+    , bodyParser = require('body-parser')
+    , methodOverride = require('method-override')
+    , session = require('express-session')
+    , partials = require('express-partials')
+    , InstagramStrategy = require('passport-instagram').Strategy;
+
+var credentials = require('./credentials.json');
+console.log(credentials.INSTAGRAM_CLIENT_ID);
+
+
+// var INSTAGRAM_CLIENT_ID = "--insert-instagram-client-id-here--";
+// var INSTAGRAM_CLIENT_SECRET = "--insert-instagram-client-secret-here--";
+var INSTAGRAM_CLIENT_ID = credentials.INSTAGRAM_CLIENT_ID,
+    INSTAGRAM_CLIENT_SECRET = credentials.INSTAGRAM_CLIENT_SECRET;
 
 
 // Passport session setup.
@@ -15,11 +27,11 @@ var INSTAGRAM_CLIENT_SECRET = "--insert-instagram-client-secret-here--";
 //   have a database of user records, the complete Instagram profile is
 //   serialized and deserialized.
 passport.serializeUser(function(user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+    done(null, obj);
 });
 
 
@@ -28,56 +40,77 @@ passport.deserializeUser(function(obj, done) {
 //   credentials (in this case, an accessToken, refreshToken, and Instagram
 //   profile), and invoke a callback with a user object.
 passport.use(new InstagramStrategy({
-    clientID: INSTAGRAM_CLIENT_ID,
-    clientSecret: INSTAGRAM_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/instagram/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // To keep the example simple, the user's Instagram profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Instagram account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
+        clientID: INSTAGRAM_CLIENT_ID,
+        clientSecret: INSTAGRAM_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Instagram profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Instagram account with a user record in your database,
+            // and return that user instead.
+            return done(null, profile);
+        });
+    }
 ));
 
 
 
 
-var app = express.createServer();
+var app = express();
 
 // configure Express
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.logger());
-  app.use(express.cookieParser());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.session({ secret: 'keyboard cat' }));
-  // Initialize Passport!  Also use passport.session() middleware, to support
-  // persistent login sessions (recommended).
-  app.use(passport.initialize());
-  app.use(passport.session());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(morgan('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded( { extended: true } ) );
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'secret'
+}));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+// configure Express
+// app.configure(function() {
+//   app.use(partials());
+//   app.set('views', __dirname + '/views');
+//   app.set('view engine', 'ejs');
+//   app.use(express.logger());
+//   app.use(express.cookieParser());
+//   app.use(express.bodyParser());
+//   // app.use(express.methodOverride());
+//   app.use(express.session({ secret: 'keyboard cat' }));
+//   // Initialize Passport!  Also use passport.session() middleware, to support
+//   // persistent login sessions (recommended).
+//   app.use(passport.initialize());
+//   app.use(passport.session());
+//   app.use(app.router);
+//   app.use(express.static(__dirname + '/public'));
+// });
 
 
 app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+    res.render('index', { user: req.user });
 });
 
 app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
+    res.render('account', { user: req.user });
 });
 
 app.get('/login', function(req, res){
-  res.render('login', { user: req.user });
+    res.render('login', { user: req.user });
 });
 
 // GET /auth/instagram
@@ -86,29 +119,36 @@ app.get('/login', function(req, res){
 //   redirecting the user to instagram.com.  After authorization, Instagram
 //   will redirect the user back to this application at /auth/instagram/callback
 app.get('/auth/instagram',
-  passport.authenticate('instagram'),
-  function(req, res){
-    // The request will be redirected to Instagram for authentication, so this
-    // function will not be called.
-  });
+    passport.authenticate('instagram'),
+    function(req, res){
+        // The request will be redirected to Instagram for authentication, so this
+        // function will not be called.
+    });
 
-// GET /auth/instagram/callback
+// GET /callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get('/auth/instagram/callback', 
-  passport.authenticate('instagram', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
+app.get('/callback',
+    passport.authenticate('instagram', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/');
+    });
 
 app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
-app.listen(3000);
+var port = 3000;
+app.listen(port, function(error) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    }
+});
 
 
 // Simple route middleware to ensure user is authenticated.
@@ -117,6 +157,6 @@ app.listen(3000);
 //   the request will proceed.  Otherwise, the user will be redirected to the
 //   login page.
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/login')
 }
